40b40af05cb47fd5e7b3d96346b6b15fc4ccea45
nicolas.bertillod <nicolas.bertillod@iquall.net>
2023-04-04 21:40:49 -0300

    Testing docs auto build

--- .husky/swagger/build.sh
+++ .husky/swagger/build.sh
@@ -1,3 +1,4 @@
 echo "Building API Docs"
 cd "$(dirname "$0")/../" && swagger-cli bundle ../swagger/index.yaml --outfile ../docs/swagger.yaml
+git add ../docs/swagger.yaml
 echo "Finished Building API Docs"
