1766a9f78215c80240fe5d16db824dfeee5bf554
nicolas.bertillod <nicolas.bertillod@iquall.net>
2023-04-13 20:49:44 -0300

    fix build.sh

--- .husky/swagger/build.sh
+++ .husky/swagger/build.sh
@@ -1,4 +1,4 @@
 echo "📖 Building API Docs 📖"
 cd "$(dirname "$0")/../" && swagger-cli bundle ../docs/index.yaml --outfile ../docs/swagger.json
-git add ../docs/swagger.yaml
+git add ../docs/swagger.json
 echo "📖 Finished Building API Docs 📖"
