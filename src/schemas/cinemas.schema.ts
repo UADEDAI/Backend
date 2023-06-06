add945589492df7f2e42521c84984d395af0330f
nicolas.bertillod <nicolas.bertillod@iquall.net>
2023-06-06 20:42:30 -0300

    add cinemas schema & correcciones 1er entrega

---
+++ src/schemas/cinemas.schema.ts
@@ -0,0 +1,102 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    CreatedAt,
+    UpdatedAt,
+    ForeignKey,
+    BelongsTo,
+    Unique,
+} from 'sequelize-typescript';
+import { User } from './users.schema';
+  
+@Table
+export class Cinema extends Model {
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      })
+    id: number;
+
+    @Unique('user_cinema_unique')
+    @ForeignKey(() => User)
+    @Column({
+      field: 'user_id',
+      type: DataType.INTEGER,
+      allowNull: false,
+    })
+    user_id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    name: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    street: string;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    street_num: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    state: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    city: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    country: string;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false,
+    })
+    latitude: number;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false,
+    })
+    longitude: number;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false,
+    })
+    price: number;
+      
+    @Column({
+        type: DataType.ENUM,
+        values: ['0', '1'],
+        allowNull: true,
+      })
+    enabled: '0' | '1';
+
+    @Column({ field: 'created_at', type: DataType.DATE })
+    createdAt: Date;
+
+    @Column({ field: 'updated_at', type: DataType.DATE })
+    updatedAt: Date;
+
+    @BelongsTo(() => User)
+    user: User;  
+}
