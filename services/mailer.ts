69083e78ae5225a919fce48328ecd19ff6ab4da1
nicolas.bertillod <nicolas.bertillod@iquall.net>
2023-06-27 21:08:39 -0300

    Renewed tokens manually

--- services/mailer.ts
+++ services/mailer.ts
@@ -5,13 +5,13 @@ const OAuth2 = google.auth.OAuth2;
 
 export async function sendEmail(to: string, subject: string, text: string, html: string): Promise<void> {
     const oauth2Client = new OAuth2(
-        '508787215655-p9n86k0eru4mr2v0lp3c1hhjh85p81j9.apps.googleusercontent.com', // Replace with your own
-        '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', // Replace with your own
+        '528611451211-e4jm249pt2jpic47d3agt0hj62pkgnbb.apps.googleusercontent.com', // Replace with your own
+        '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', // Replace with your own
         'https://developers.google.com/oauthplayground' // Redirect URL
     );
 
     oauth2Client.setCredentials({
-        'refresh_token': '1//040qhhSMXxeNWCgYIARAAGAQSNwF-L9IrO1cKGjjFanZJt2gnAGns6QEuEJjDrS1Xod2IhgQhnstS_q5vNFOvrAyotXzViaWybvg' // Replace with your own
+        'refresh_token': '1//04jCqe2tY4uuKCgYIARAAGAQSNwF-L9IrRK1P9CfnjCK9BceBkotjBFvlD0onDye_y8OXhDdA2AzPIyh6JYBxjc_97-rci-ZI6Kk' // Replace with your own
     });
 
     const accessToken = oauth2Client.getAccessToken()
@@ -21,9 +21,9 @@ export async function sendEmail(to: string, subject: string, text: string, html:
         auth: {
             type: 'OAuth2',
             user: 'cinemappdai@gmail.com', // Replace with your email
-            clientId: '508787215655-p9n86k0eru4mr2v0lp3c1hhjh85p81j9.apps.googleusercontent.com', // Replace with your own
-            clientSecret: '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', // Replace with your own
-            refreshToken: '1//040qhhSMXxeNWCgYIARAAGAQSNwF-L9IrO1cKGjjFanZJt2gnAGns6QEuEJjDrS1Xod2IhgQhnstS_q5vNFOvrAyotXzViaWybvg', // Replace with your own
+            clientId: '528611451211-e4jm249pt2jpic47d3agt0hj62pkgnbb.apps.googleusercontent.com', // Replace with your own
+            clientSecret: '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', // Replace with your own
+            refreshToken: '1//04jCqe2tY4uuKCgYIARAAGAQSNwF-L9IrRK1P9CfnjCK9BceBkotjBFvlD0onDye_y8OXhDdA2AzPIyh6JYBxjc_97-rci-ZI6Kk', // Replace with your own
             accessToken: accessToken
         }
     });
